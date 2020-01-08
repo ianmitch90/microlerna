ed2a36f95517a93156654847c004c20d8877b0d3
ianmitch90 <you@example.com>
2020-01-08 11:27:32 -0500

    removed some extra components, made some dependency changes, added wrapped inputs

---
+++ packages/web/constants/ValidationMessages.ts
@@ -0,0 +1,14 @@
+export default {
+  ERROR: "Incorrect input value",
+  STRING: "Input only accepts alphanumeric values",
+  NUMBER: "Input only accepts number values",
+  LONG: "Input length is too long",
+  SHORT: "Input length is too short",
+  MIN: "Input value is less than requirement",
+  MAX: "Input value is more than requirement",
+  REQUIRED: "Input is required",
+  FORMAT: "Input format incorrect",
+  DATE: "Incorrect date",
+  DATEMIN: "Date too far in past",
+  DATEMAX: "Date too far in future"
+};
